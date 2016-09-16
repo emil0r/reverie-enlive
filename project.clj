1f42438f88499d976086631ccd7beb853246d05f
Emil Bengtsson <emil@emil0r.com>
2016-09-16 22:24:36 +0200

    0.2.0
    changes made according to the new renderer functionality in reverie

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject reverie-enlive "0.1.0"
+(defproject reverie-enlive "0.2.0"
   :description "Render function for reverie/CMS that supports enlive."
   :url "http://reveriecms.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [enlive "1.1.6"]
-                 [hiccup "1.0.5"]])
+                 [enlive "1.1.6"]])
--- src/reverie/batteries/enlive.clj
+++ src/reverie/batteries/enlive.clj
@@ -1,22 +1,10 @@
 (ns reverie.batteries.enlive
   "render-fn for enlive/hiccup mix"
   (:require
-   [hiccup.compiler        :refer [render-html]]
-   [net.cgrand.enlive-html :refer [emit*]]))
+   [net.cgrand.enlive-html :refer [emit*]]
+   [reverie.render :refer [get-render-fn]]))
 
-(defn render-fn [data]
-  (cond
-    ;; enlive data?
-    (and (seq? data)
-         (map? (first data))
-         (contains? (first data) :tag)
-         (contains? (first data) :attrs)
-         (contains? (first data) :content)) (emit* data)
 
-    (and (map? data)
-         (contains? data :tag)
-         (contains? data :attrs)
-         (contains? data :content)) (emit* data)
 
-    ;; assume hiccup data
-    :else (render-html data)))
+(defmethod get-render-fn :enlive [_]
+  net.cgrand.enlive-html/emit*)
